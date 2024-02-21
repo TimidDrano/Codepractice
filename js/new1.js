b7b242740a9f39463c90a98682205df7e8fcc4b7
Chard <rmwambanga@outlook.com>
2024-02-21 22:37:47 +0300

    Settingup_vscode_terminal

--- js/new1.js
+++ js/new1.js
@@ -1,3 +1,17 @@
+// Wait for 3000 milliseconds (3s) and then show the overlay
+
+/*setTimeout(function (){
+  alert("Hey there");
+}, 3000);
+*/
+
+// Function to open the overlay
+/*
+function overlay() {
+    document.getElementById("myOverlay").style.width = "100%";
+}
+*/
+
 const prompt = require('prompt-sync')();
 
 var fname = prompt("Enter your 1st Name:");
